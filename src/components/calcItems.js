372da4175156024f95a5a36d44380a124d7a7ab6
meryemsanem <meryemsanemkucukbayram@gmail.com>
2023-07-11 15:17:12 +0300

    Add style for the calc container

--- src/components/calcItems.js
+++ src/components/calcItems.js
@@ -1,21 +1,24 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import './Calculator.css';
+import './calcItems.css';
 
 const Items = ({ number }) => {
   if (
-    number === '\u00F7'
-    || number === 'x'
-    || number === '-'
-    || number === '+'
-    || number === '='
+    number === '\u00F7' ||
+    number === 'x' ||
+    number === '-' ||
+    number === '+' ||
+    number === '='
   ) {
-    return <div className="input-button signs">{number}</div>;
+    return <div className="input-btn signs">{number}</div>;
   }
   if (number === '0') {
-    return <div className="input-button zero">{number}</div>;
+    return <div className="input-btn zero">{number}</div>;
   }
-  return <div className="input-button">{number}</div>;
+  if (number === 'AC' || number === '+/-' || number === '%') {
+    return <div className="input-btn top">{number}</div>;
+  }
+  return <div className="input-btn">{number}</div>;
 };
 
 Items.propTypes = {
